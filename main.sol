54671d6a987fbd49b9acaddf20c30c7bf4d607a6
"Aloysius" <"aloycwl@gmail.com">
2022-06-03 23:21:29 +0800

    comment

--- main.sol
+++ main.sol
@@ -13,7 +13,7 @@ contract ERC721_LSG is ERC721{
     function MINT(string[] memory r)external{unchecked{
         require(r.length<6);
         require(count<3334);
-        require(balanceOf(msg.sender)+r.length<5);
+        require(balanceOf(msg.sender)+r.length<6);
         for(uint i=0;i<r.length;i++){
             count++;
             if(bytes(_uri[count]).length<1)_uri[count]=r[i];
@@ -21,3 +21,5 @@ contract ERC721_LSG is ERC721{
         }
     }}
 }
+// ["QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/8.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/69.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/188.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/424.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/666.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/823.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/967.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/1316.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/2022.json","QmZAmBtadVezuFj3u3BvZPQZ3Jc9XhdD7TrxpNJbzNyX83/3278.json"]
+// [8,69,188,424,666,823,967,1316,2022,3278]
