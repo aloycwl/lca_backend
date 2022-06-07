f4bb61a9a724520095929680dceb265353fc7529
"Aloysius" <"aloycwl@gmail.com">
2022-06-07 10:01:04 +0800

    Updated

--- main.sol
+++ main.sol
@@ -9,9 +9,18 @@ contract ERC721_LSG is ERC721AC{
     function symbol()external pure override returns(string memory){
         return"LSG";
     }
-    function tokenURI(uint a)external pure override returns(string memory){
-        return string(abi.encodePacked("ipfs://",a));
-    }
+    function tokenURI(uint a)external pure override returns(string memory){unchecked{
+        string memory b="0";
+        if(a>0){
+            uint256 temp=a;
+            uint256 digits;
+            while(temp!=0)(digits++,temp/=10);
+            bytes memory buffer=new bytes(digits);
+            while(a!=0)(digits-=1,buffer[digits]=bytes1(uint8(48+uint256(a%10))),a/=10);
+            b=string(buffer);
+        }
+        return string(abi.encodePacked("ipfs://",b));
+    }}
     function MINT(uint n)external{unchecked{
         require(_balances[msg.sender]+n<6);
         require(count<3334);
